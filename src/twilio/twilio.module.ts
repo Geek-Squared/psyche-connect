a449cab7738e64253669486b29f1014e4476f48e
Moses Ngwerume <moses.ngwerume@wyzetalk.com>
2025-01-02 09:54:34 +0200

    removed twilio folder

--- src/twilio/twilio.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TwilioService } from './twilio.service';
-import { TwilioController } from './twilio.controller';
-
-@Module({
-  providers: [TwilioService],
-  controllers: [TwilioController]
-})
-export class TwilioModule {}
